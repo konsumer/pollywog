798b92a30f1ab53f2065977f234a62331c5bfdb0
David Konsumer <konsumer@metaltoad.com>
2012-04-03 11:38:01 -0700

    added sessionstorage caching

--- src/shared/microtemplate.js
+++ src/shared/microtemplate.js
@@ -2,46 +2,44 @@
 
 // Simple JavaScript Templating
 // John Resig - http://ejohn.org/ - MIT Licensed
-// pollywog adds markdown, html cleaning & raw() (no cleaning) & error-handling & single-quote + newline support
+// pollywog adds markdown, html cleaning & raw() (no cleaning) & error-handling & single-quote + newline support & sessionStorage caching
 define(['share/util', 'share/showdown'], function(util, showdown, md5){
-  var cache={};
-
   var tmpl = function(key, t, data){
     // Figure out if we're getting a template, or if we need to
     // load the template - and be sure to cache the result.
     var fn, str;
-    
+
     try{
-      if (cache[key] === undefined){
+      if (window.sessionStorage[key] === undefined){
         str = t;
         if (key.substr(-2) == 'md'){
           str = showdown.makeHtml(t);
         }
+        fn = new Function("obj",
+          "var p=[],print=function(){p.push.apply(p,arguments);};" +
+          "with(obj){p.push('" +
+          str
+            .replace(/'/g,'&rsquo;')
+            .replace(/†/g, "&dagger;")
+            .replace(/[\r\n]/g, "†")
+            .replace(/\t/g, "  ")
+            .split("<%").join("\t")
+            .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+            .replace(/\t=raw (.*?)%>/g, "',$1,'")
+            .replace(/\t=(.*?)%>/g, "',require(\"share/util\").clean($1),'")
+            .split("\t").join("');")
+            .split("%>").join("p.push('")
+            .split("\r").join("\\'") +
+
+
+          // Introduce the data as local variables using with(){}
+          "');}return p.join('').replace(/†/g,\"\\n\");");
+
+        window.sessionStorage[key] = fn.toString() + '; return anonymous(obj);';
+      }else{
+         fn = new Function("obj", window.sessionStorage[key]);
+         console.log(fn);
       }
-
-      // Generate a reusable function that will serve as a template
-      // generator (and which will be cached).
-      fn = cache[key] || new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
-        "with(obj){p.push('" +
-        str
-          .replace(/'/g,'&rsquo;')
-          .replace(/†/g, "&dagger;")
-          .replace(/[\r\n]/g, "†")
-          .replace(/\t/g, "  ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=raw (.*?)%>/g, "',$1,'")
-          .replace(/\t=(.*?)%>/g, "',require(\"share/util\").clean($1),'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'") +
-
-
-        // Introduce the data as local variables using with(){}
-        "');}return p.join('').replace(/†/g,\"\\n\");");
-
-      cache[key] = fn;
 
       // Provide some basic currying to the user
       return data ? fn( data ) : fn;
